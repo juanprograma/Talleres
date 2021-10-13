ad5b3341b2eda5bd40aca61a00d43ac7acf109c2
juanprograma <69698438+juanprograma@users.noreply.github.com>
2021-10-13 13:15:32 -0500

    Add files via upload

---
+++ TallerTypescript - J/TallerTypescript - J/course.js
@@ -0,0 +1,10 @@
+
+var Course = /** @class */ (function () {
+    function Course(name, professor, credits) {
+        this.name = name;
+        this.credits = credits;
+        this.professor = professor;
+    }
+    return Course;
+}());
+export {Course};
